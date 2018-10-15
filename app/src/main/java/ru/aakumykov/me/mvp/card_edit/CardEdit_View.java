8b29840c3fac1210154f62763dc60644cfe946cc
Андрей Кумыков <aakumykov@yandex.ru>
2018-10-15 11:55:55 +0700

    Нарушил MVP, буду исправлять.

--- app/src/main/java/ru/aakumykov/me/mvp/card_edit/CardEdit_View.java
+++ app/src/main/java/ru/aakumykov/me/mvp/card_edit/CardEdit_View.java
@@ -1,25 +1,39 @@
 package ru.aakumykov.me.mvp.card_edit;
 
+import android.Manifest;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
-import com.google.firebase.database.DatabaseReference;
+import com.google.android.gms.tasks.OnCanceledListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageMetadata;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
+
+import java.security.SecureRandom;
+import java.security.SecureRandomSpi;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import permissions.dispatcher.NeedsPermission;
 import ru.aakumykov.me.mvp.Constants;
 import ru.aakumykov.me.mvp.MyUtils;
 import ru.aakumykov.me.mvp.R;
@@ -30,11 +44,6 @@ import ru.aakumykov.me.mvp.models.Card;
 public class CardEdit_View extends AppCompatActivity
         implements iCardEdit.View, iCardEdit.ModelCallbacks {
 
-    private final static String TAG = "CardEdit_View";
-    private Card currentCard;
-    private iCardEdit.Model model = CardEdit_Model.getInstance();
-    private FirebaseStorage firebaseStorage = FirebaseStorage.getInstance();
-
     @BindView(R.id.progressBar) ProgressBar progressBar;
     @BindView(R.id.messageView) TextView messageView;
     @BindView(R.id.titleView) EditText titleView;
@@ -42,9 +51,18 @@ public class CardEdit_View extends AppCompatActivity
     @BindView(R.id.imageHolder) ConstraintLayout imageHolder;
     @BindView(R.id.imageView) ImageView imageView;
     @BindView(R.id.imageProgressBar) ProgressBar imageProgressBar;
+    @BindView(R.id.imageDeleteButton) ImageButton imageDeleteButton;
     @BindView(R.id.descriptionView) EditText descriptionView;
     @BindView(R.id.saveButton) Button saveButton;
     @BindView(R.id.cancelButton) Button cancelButton;
+
+
+    private final static String TAG = "CardEdit_View";
+    private iCardEdit.Model model = CardEdit_Model.getInstance();
+    private FirebaseStorage firebaseStorage = FirebaseStorage.getInstance();
+
+    private Card currentCard;
+    private String oldImageURL;
 
 
     @Override
@@ -80,11 +98,14 @@ public class CardEdit_View extends AppCompatActivity
 
             case Constants.IMAGE_CARD:
                 Log.d(TAG, "графическая карточка");
+
                 MyUtils.show(imageHolder);
+
                 loadImage(card.getImageURL(), new Callback() {
                     @Override
                     public void onSuccess() {
                         MyUtils.hide(imageProgressBar);
+                        MyUtils.show(imageDeleteButton);
                     }
 
                     @Override
@@ -104,6 +125,58 @@ public class CardEdit_View extends AppCompatActivity
         enableEditForm();
     }
 
+    @OnClick(R.id.imageDeleteButton)
+    void deleteImage() {
+        Log.d(TAG, "deleteImage()");
+        oldImageURL = currentCard.getImageURL();
+        currentCard.setImageURL(null);
+
+        imageView.setImageDrawable(getResources().getDrawable(R.drawable.ic_picture_placeholder));
+        MyUtils.hide(imageDeleteButton);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                slectImage();
+            }
+        });
+    }
+
+    @NeedsPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+    private void slectImage() {
+//        Log.d(TAG, "selectImage()");
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+
+        if (null != intent.resolveActivity(getPackageManager())) {
+            startActivityForResult(
+                    Intent.createChooser(intent, getResources().getString(R.string.select_image)),
+                    Constants.CODE_SELECT_IMAGE
+            );
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (RESULT_OK == resultCode) {
+            switch (requestCode) {
+                case Constants.CODE_SELECT_IMAGE:
+                    if (null != data) {
+                        Uri dataURI = data.getData();
+                        displayLocalImage(dataURI);
+                    }
+                    break;
+                default:
+                    Log.w(TAG, "Unknown request code: "+requestCode);
+            }
+        } else {
+            Log.w(TAG, "Unsuccessful result code");
+        }
+    }
+
+
 
     // TODO: сохранение в виде транзакции (и/или блокировка на время правки)
     @OnClick(R.id.saveButton)
@@ -115,7 +188,7 @@ public class CardEdit_View extends AppCompatActivity
         currentCard.setDescription(descriptionView.getText().toString());
 
         disableEditForm();
-        showMessage(R.string.savingCard, Constants.INFO_MSG);
+        showMessage(R.string.saving_card, Constants.INFO_MSG);
         showProgressBar();
 
         model.saveCard(currentCard, this);
@@ -135,14 +208,14 @@ public class CardEdit_View extends AppCompatActivity
         Log.d(TAG, "onSaveError()");
         hideProgressBar();
         enableEditForm();
-        showMessage(R.string.savingError, Constants.ERROR_MSG);
+        showMessage(R.string.saving_error, Constants.ERROR_MSG);
     }
 
     @Override
     public void onSaveCancel() {
         hideProgressBar();
         enableEditForm();
-        showMessage(R.string.savingCancel, Constants.ERROR_MSG);
+        showMessage(R.string.saving_cancel, Constants.ERROR_MSG);
     }
 
 
@@ -236,5 +309,46 @@ public class CardEdit_View extends AppCompatActivity
                     }
                 });
 
+    }
+
+    private void displayLocalImage(Uri imageURI) {
+        Log.d(TAG, "displayLocalImage("+imageURI+")");
+        Picasso.get().load(imageURI).into(imageView);
+    }
+
+    private void uploadImage(Uri imageURI, final Callback callback) {
+        Log.d(TAG, "uploadImage()");
+
+        // TODO: разобраться с типом файла
+        String fileName = currentCard.getKey() + ".jpg";
+
+        StorageReference imageRef = firebaseStorage.getReference()
+                .child(Constants.IMAGES_PATH);
+
+        StorageMetadata metadata = new StorageMetadata.Builder()
+                .setContentType("image/jpeg")
+                .build();
+
+        UploadTask uploadTask = imageRef.putFile(imageURI, metadata);
+
+        uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        callback.onSuccess();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        callback.onError(e);
+                    }
+                })
+                .addOnCanceledListener(new OnCanceledListener() {
+                    @Override
+                    public void onCanceled() {
+                        Log.w(TAG, "Image upload cancelled");
+                        deleteImage();
+                    }
+                });
     }
 }
