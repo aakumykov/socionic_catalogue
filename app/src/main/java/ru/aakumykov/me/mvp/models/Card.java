0c1dc8019904a992197e7b094e79842201369763
Андрей Кумыков <aakumykov@yandex.ru>
2018-10-27 12:17:11 +0700

    Правильно добавил поле меток в модель Card.

--- app/src/main/java/ru/aakumykov/me/mvp/models/Card.java
+++ app/src/main/java/ru/aakumykov/me/mvp/models/Card.java
@@ -3,6 +3,11 @@ package ru.aakumykov.me.mvp.models;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+import com.google.firebase.database.Exclude;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Card implements Parcelable {
 
     private String key;
@@ -11,25 +16,45 @@ public class Card implements Parcelable {
     private String quote;
     private String imageURL;
     private String description;
+    private List<String> tags = new ArrayList<>();
 
     public Card() {
     }
 
-    public Card(String type, String title, String quote, String imageURL, String description) {
+    public Card(String type, String title, String quote, String imageURL, String description, List<String> tagsList) {
         this.type = type;
         this.title = title;
         this.quote = quote;
         this.imageURL = imageURL;
         this.description = description;
+        this.tags = tagsList;
     }
 
     @Override
     public String toString() {
-        return "Card { key: "+getKey()+", title: "+getTitle()+", quote: "+getQuote()+", imageURL: "+imageURL+", description: "+getDescription()+",}";
+        return "Card { key: "+getKey()+
+                ", title: "+getTitle()+
+                ", quote: "+getQuote()+
+                ", imageURL: "+imageURL+
+                ", description: "+getDescription()+
+                ", tags: "+getTags()+
+            ",}";
     }
 
 
     /* Parcelable */
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        // порядок заполнения важен
+        dest.writeString(this.key);
+        dest.writeString(this.type);
+        dest.writeString(this.title);
+        dest.writeString(this.quote);
+        dest.writeString(this.imageURL);
+        dest.writeString(this.description);
+        dest.writeStringList(this.tags);
+    }
+
     protected Card(Parcel in) {
         key = in.readString();
         type = in.readString();
@@ -37,6 +62,7 @@ public class Card implements Parcelable {
         quote = in.readString();
         imageURL = in.readString();
         description = in.readString();
+        in.readStringList(this.tags);  // Вот это да!
     }
 
     @Override
@@ -55,17 +81,6 @@ public class Card implements Parcelable {
             return new Card[size];
         }
     };
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        // порядок заполнения важен
-        dest.writeString(this.key);
-        dest.writeString(this.type);
-        dest.writeString(this.title);
-        dest.writeString(this.quote);
-        dest.writeString(this.imageURL);
-        dest.writeString(this.description);
-    }
     /* Parcelable */
 
 
@@ -87,6 +102,7 @@ public class Card implements Parcelable {
     public String getDescription() {
         return description;
     }
+    public List<String> getTags() { return this.tags; }
 
     public void setKey(String key) {
         this.key = key;
@@ -106,5 +122,14 @@ public class Card implements Parcelable {
     public void setDescription(String description) {
         this.description = description;
     }
+    public void setTags(List<String> tagsList) { this.tags = tagsList; }
 
+//    public List<String> getTags() {
+//        List<String> tagsList = new ArrayList<>();
+//        tagsList.add("Метка-1");
+//        tagsList.add("Метка-2");
+//        tagsList.add("Метка-3");
+//        tagsList.add("Метка-4");
+//        return tagsList;
+//    }
 }
