5977930bc80b4761536aeca9ec80844c820b10e3
Андрей Кумыков <aakumykov@yandex.ru>
2018-10-27 10:12:32 +0700

    Переусложнение Card (признак кривой архитектуры).

--- app/src/main/java/ru/aakumykov/me/mvp/models/Card.java
+++ app/src/main/java/ru/aakumykov/me/mvp/models/Card.java
@@ -2,8 +2,18 @@ package ru.aakumykov.me.mvp.models;
 
 import android.os.Parcel;
 import android.os.Parcelable;
+import android.util.Log;
+
+import com.google.firebase.database.Exclude;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class Card implements Parcelable {
+
+    @Exclude
+    private final static String TAG = "Card";
 
     private String key;
     private String type;
@@ -11,25 +21,46 @@ public class Card implements Parcelable {
     private String quote;
     private String imageURL;
     private String description;
+    private HashMap<String, Boolean> tags;
 
     public Card() {
     }
 
-    public Card(String type, String title, String quote, String imageURL, String description) {
+    public Card(String type, String title, String quote, String imageURL, String description, List<String> tagsList) {
         this.type = type;
         this.title = title;
         this.quote = quote;
         this.imageURL = imageURL;
         this.description = description;
+        this.setTags(tagsList);
     }
 
     @Override
     public String toString() {
-        return "Card { key: "+getKey()+", title: "+getTitle()+", quote: "+getQuote()+", imageURL: "+imageURL+", description: "+getDescription()+",}";
+        return "Card { " +
+                "key: "+getKey()+
+                ", title: "+getTitle()+
+                ", quote: "+getQuote()+
+                ", imageURL: "+imageURL+
+                ", description: "+getDescription()+
+                ", tags: "+getTags()+
+            ",}";
     }
 
 
     /* Parcelable */
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        // порядок заполнения важен
+        dest.writeString(this.key);
+        dest.writeString(this.type);
+        dest.writeString(this.title);
+        dest.writeString(this.quote);
+        dest.writeString(this.imageURL);
+        dest.writeString(this.description);
+        dest.writeStringArray();
+    }
+
     protected Card(Parcel in) {
         key = in.readString();
         type = in.readString();
@@ -55,20 +86,10 @@ public class Card implements Parcelable {
             return new Card[size];
         }
     };
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        // порядок заполнения важен
-        dest.writeString(this.key);
-        dest.writeString(this.type);
-        dest.writeString(this.title);
-        dest.writeString(this.quote);
-        dest.writeString(this.imageURL);
-        dest.writeString(this.description);
-    }
     /* Parcelable */
 
 
+    // Простые геттеры/сеттеры
     public String getKey() {
         return key;
     }
@@ -107,4 +128,20 @@ public class Card implements Parcelable {
         this.description = description;
     }
 
+
+    // Сложные геттеры/сеттеры
+    public List<String> getTags() {
+        List<String> list = new ArrayList<>(this.tags.keySet());
+        String[] array = new String[list.size()];
+        /* Вот эта сложная хуета с преобразованиями меток
+        * требуется потому, что я передаю Карточку через Intent ... */
+    }
+
+    public void setTags(List<String> tagsList) {
+        Log.d(TAG, "setTags(), "+tagsList);
+
+        for (String tagName : tagsList) {
+            this.tags.put(tagName, true);
+        }
+    }
 }
