2d09242a5322e451c1e05a0755d91a28fdfc5de8
Андрей Кумыков <aakumykov@yandex.ru>
2018-11-12 23:40:16 +0700

    Убрал лишнее из CardsSingleton.

--- app/src/main/java/ru/aakumykov/me/mvp/services/CardsSingleton.java
+++ app/src/main/java/ru/aakumykov/me/mvp/services/CardsSingleton.java
@@ -1,26 +1,17 @@
 package ru.aakumykov.me.mvp.services;
 
-import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
-import com.google.android.gms.tasks.OnCanceledListener;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.OnProgressListener;
-import com.google.firebase.storage.StorageMetadata;
-import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -45,14 +36,8 @@ public class CardsSingleton implements
 
     // Свойства
     private final static String TAG = "CardsSingleton";
-
     private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
-    private FirebaseStorage firebaseStorage = FirebaseStorage.getInstance();
-
     private DatabaseReference cardsRef = firebaseDatabase.getReference().child(Constants.CARDS_PATH);
-    private StorageReference imagesRef = firebaseStorage.getReference().child(Constants.IMAGES_PATH);
-
-    private UploadTask uploadTask;
 
 
     // Интерфейсные методы
