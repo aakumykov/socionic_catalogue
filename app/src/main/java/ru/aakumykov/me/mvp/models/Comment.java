1fb5887b015e9fdbb20e3637f31eb9ce2306a4af
Андрей Кумыков <aakumykov@yandex.ru>
2018-11-12 23:52:35 +0700

    Заготовка модели Comment.

---
+++ app/src/main/java/ru/aakumykov/me/mvp/models/Comment.java
@@ -0,0 +1,105 @@
+package ru.aakumykov.me.mvp.models;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.google.firebase.database.Exclude;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Comment implements Parcelable {
+
+    private String key;
+    private String text;
+
+    private String cardId;
+    private String parentId;
+    private String userId;
+    private String createdAt;
+    private String editedAt;
+    private int rating = 0;
+    
+    public Comment(){}
+    
+    public Comment(String text, String cardId, String parentId, String userId) {
+        this.text = text;
+        this.cardId = cardId;
+        this.parentId = parentId;
+        this.userId = userId;
+    }
+    
+
+    @Override @Exclude
+    public String toString() {
+        return "Comment { "
+                +"key: "+key
+                +"text: "+text
+                +"cardId: "+cardId
+                +"parentId: "+parentId
+                +"userId: "+userId
+                +"createdAt: "+createdAt
+                +"editedAt: "+editedAt
+                +"rating: "+rating
+            +" }";
+    }
+
+    @Exclude
+    public Map<String, Object> toMap() {
+        HashMap<String, Object> map = new HashMap<>();
+            map.put("key", key);
+            map.put("text", text);
+            map.put("cardId", cardId);
+            map.put("parentId", parentId);
+            map.put("userId", userId);
+            map.put("createdAt", createdAt);
+            map.put("editedAt", editedAt);
+            map.put("rating", rating);
+        return map;
+    }
+
+
+    /* Parcelable */
+    public static final Creator<Comment> CREATOR = new Creator<Comment>() {
+        @Override
+        public Comment createFromParcel(Parcel in) {
+            return new Comment(in);
+        }
+
+        @Override
+        public Comment[] newArray(int size) {
+            return new Comment[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        // важен порядок заполнения
+        dest.writeString(key);
+        dest.writeString(text);
+        dest.writeString(cardId);
+        dest.writeString(parentId);
+        dest.writeString(userId);
+        dest.writeString(createdAt);
+        dest.writeString(editedAt);
+        dest.writeInt(rating);
+    }
+
+    private Comment(Parcel in) {
+        // важен порядок считывания
+        key = in.readString();
+        text = in.readString();
+        cardId = in.readString();
+        parentId = in.readString();
+        userId = in.readString();
+        createdAt = in.readString();
+        editedAt = in.readString();
+        rating = in.readInt();
+    }
+    /* Parcelable */
+}
