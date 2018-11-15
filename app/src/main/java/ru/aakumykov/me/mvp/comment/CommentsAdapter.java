cc4ac726ead671797d3ef485e1612011cb31a297
Андрей Кумыков <aakumykov@yandex.ru>
2018-11-15 19:13:02 +0700

    Пошёл не в ту степь в реализации щелчка по комменту.

--- app/src/main/java/ru/aakumykov/me/mvp/comment/CommentsAdapter.java
+++ app/src/main/java/ru/aakumykov/me/mvp/comment/CommentsAdapter.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.List;
@@ -13,8 +14,8 @@ import java.util.List;
 import ru.aakumykov.me.mvp.R;
 import ru.aakumykov.me.mvp.models.Comment;
 
-public class CommentsAdapter extends ArrayAdapter<Comment> {
-
+public class CommentsAdapter extends ArrayAdapter<Comment>
+{
     // TODO: реализовать ViewHolder
 
     private final static String TAG = "CommentsAdapter";
@@ -22,7 +23,9 @@ public class CommentsAdapter extends ArrayAdapter<Comment> {
     private int layout;
     private List<Comment> list;
 
-    public CommentsAdapter(Context context, int resource, List<Comment> commentsList) {
+    public CommentsAdapter(Context context, int resource,
+                           List<Comment> commentsList, iComments.clickListener clickListener)
+    {
         super(context, resource, commentsList);
         this.list = commentsList;
         this.layout = resource;
@@ -32,11 +35,19 @@ public class CommentsAdapter extends ArrayAdapter<Comment> {
     @NonNull
     public View getView(int position, View convertView, @NonNull ViewGroup parent) {
 
+        Comment comment = list.get(position);
+
         View view = inflater.inflate(this.layout, parent, false);
         TextView commentTextView = view.findViewById(R.id.commentText);
+        ImageView commentMenu = view.findViewById(R.id.commentMenu);
 
-        Comment comment = list.get(position);
         commentTextView.setText(comment.getText());
+        commentMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
         return view;
     }
---
+++ app/src/main/java/ru/aakumykov/me/mvp/comment/iComments.java
@@ -0,0 +1,10 @@
+package ru.aakumykov.me.mvp.comment;
+
+import android.view.View;
+
+public interface iComments {
+
+    interface clickListener {
+        void onCommentMenuClicked(View view);
+    }
+}
