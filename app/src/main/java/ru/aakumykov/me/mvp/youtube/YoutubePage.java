3150b50cc0ca7ef20894748e8043ce32bf778d75
Андрей Кумыков <aakumykov@yandex.ru>
2018-11-26 19:56:06 +0700

    YoutubePage с динамическим YouTubePlayerView работает.

--- app/src/main/java/ru/aakumykov/me/mvp/youtube/YoutubePage.java
+++ app/src/main/java/ru/aakumykov/me/mvp/youtube/YoutubePage.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 import android.widget.VideoView;
@@ -24,10 +25,11 @@ import ru.aakumykov.me.mvp.utils.MVPUtils;
 
 public class YoutubePage extends BaseView {
 
-    @BindView(R.id.videoPlayerThrobber) ProgressBar videoPlayerThrobber;
-    @BindView(R.id.youTubePlayerView) YouTubePlayerView youTubePlayerView;
+    @BindView(R.id.videoPlayerHolder) FrameLayout videoPlayerHolder;
 
+    private YouTubePlayerView youTubePlayerView;
     private YouTubePlayer youTubePlayer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,9 +38,7 @@ public class YoutubePage extends BaseView {
         ButterKnife.bind(this);
 
         Toast.makeText(this, "onCreate()", Toast.LENGTH_SHORT).show();
-
         activateUpButton();
-
         playVideo();
     }
 
@@ -46,20 +46,15 @@ public class YoutubePage extends BaseView {
     protected void onDestroy() {
         super.onDestroy();
         youTubePlayerView.release();
+//        videoPlayerHolder.removeAllViews();
         Toast.makeText(this, "onDestroy()", Toast.LENGTH_SHORT).show();
     }
 
-
-    @Override
-    public void onUserLogin() {
-
-    }
-    @Override
-    public void onUserLogout() {
-
-    }
-
     void playVideo() {
+        youTubePlayerView = new YouTubePlayerView(this);
+        youTubePlayerView.setId(R.id.youtube_video_player_id);
+        videoPlayerHolder.addView(youTubePlayerView);
+
         youTubePlayerView.initialize(new YouTubePlayerInitListener() {
             @Override
             public void onInitSuccess(@NonNull final YouTubePlayer initializedYouTubePlayer) {
@@ -75,5 +70,10 @@ public class YoutubePage extends BaseView {
     }
 
 
+    @Override public void onUserLogin() {
 
+    }
+    @Override public void onUserLogout() {
+
+    }
 }
