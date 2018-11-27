1ac70b8ea71db469111fbaea8958c792ada3c717
Андрей Кумыков <aakumykov@yandex.ru>
2018-11-27 17:15:00 +0700

    fix: векторная графика в старых API (AppCompat).

--- app/src/main/java/ru/aakumykov/me/mvp/BaseView.java
+++ app/src/main/java/ru/aakumykov/me/mvp/BaseView.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -46,6 +47,8 @@ public abstract class BaseView extends AppCompatActivity implements
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
 
         // TODO: убрать вообще?
         authService = AuthSingleton.getInstance();
