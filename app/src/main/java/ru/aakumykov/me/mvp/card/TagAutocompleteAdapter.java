342347949ea91e5ef9dfe5420d9384592dfa0ba3
Андрей Кумыков <aakumykov@yandex.ru>
2018-12-30 17:44:16 +0700

    Неудачная попытка поиска по многословным меткам.

--- app/src/main/java/ru/aakumykov/me/mvp/card/TagAutocompleteAdapter.java
+++ app/src/main/java/ru/aakumykov/me/mvp/card/TagAutocompleteAdapter.java
@@ -6,8 +6,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Filter;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
@@ -19,14 +21,18 @@ public class TagAutocompleteAdapter extends ArrayAdapter<String> {
     private LayoutInflater inflater;
     private int layout;
     private List<String> list;
+    private List<String> allTags = new ArrayList<>();
+    private List<String> suggestedTags = new ArrayList<>();
 
     public TagAutocompleteAdapter(Context context, int resource, List<String> list) {
         super(context, resource, list);
         this.list = list;
+        this.allTags.addAll(this.list);
         this.layout = resource;
         this.inflater = LayoutInflater.from(context);
     }
 
+    // Разметочная машина
     @NonNull
     public View getView(int position, View convertView, @NonNull ViewGroup parent) {
 
@@ -53,4 +59,64 @@ public class TagAutocompleteAdapter extends ArrayAdapter<String> {
             ButterKnife.bind(this, view);
         }
     }
+
+    // Фильтровальная станция
+    @NonNull @Override
+    public Filter getFilter() {
+        return tagFilter;
+    }
+
+    private Filter tagFilter = new Filter() {
+
+        @Override
+        protected FilterResults performFiltering(CharSequence pattern) {
+
+            if (pattern != null) {
+                suggestedTags.clear();
+                pattern = pattern.toString().toLowerCase();
+
+                for (String tag : allTags) {
+                    tag = tag.toLowerCase();
+                    String[] tagPieces = tag.split("\\s+");
+
+                    for (String word : tagPieces) {
+                        if (word.startsWith(pattern.toString())) {
+                            suggestedTags.add(tag);
+                            break;
+                        }
+                    }
+                }
+
+                FilterResults filterResults = new FilterResults();
+                filterResults.values = suggestedTags;
+                filterResults.count = suggestedTags.size();
+                return filterResults;
+            }
+            else {
+                return new FilterResults();
+            }
+        }
+
+        @Override
+        public String convertResultToString(Object resultValue) {
+            String str = ((String)(resultValue));
+            return str;
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+
+            ArrayList<String> filteredList = (ArrayList<String>) results.values;
+
+            if (results != null && results.count > 0) {
+                clear();
+                for (String c : filteredList) {
+                    add(c);
+                }
+                notifyDataSetChanged();
+            }
+        }
+
+    };
+
 }
