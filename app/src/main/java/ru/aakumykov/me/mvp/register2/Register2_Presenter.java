e6a1c181032137fb45257b8d65f7ce7d5295e6a6
Андрей Кумыков <aakumykov@yandex.ru>
2019-01-15 08:02:58 +0700

    *

--- app/src/main/java/ru/aakumykov/me/mvp/register2/Register2_Presenter.java
+++ app/src/main/java/ru/aakumykov/me/mvp/register2/Register2_Presenter.java
@@ -2,6 +2,9 @@ package ru.aakumykov.me.mvp.register2;
 
 import android.text.TextUtils;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -14,15 +17,12 @@ import ru.aakumykov.me.mvp.services.UsersSingleton;
 
 public class Register2_Presenter implements iRegister2.Presenter {
 
-    private iAuthSingleton authService;
-    private iUsersSingleton usersService;
+    private iAuthSingleton authService = AuthSingleton.getInstance();
+    private iUsersSingleton usersService = UsersSingleton.getInstance();
+    private Map<String,Boolean> formCheckResults = new HashMap<>();
     private iRegister2.View view;
-    private boolean formIsValid = false;
 
-    Register2_Presenter() {
-        authService = AuthSingleton.getInstance();
-        usersService = UsersSingleton.getInstance();
-    }
+    Register2_Presenter() {}
 
 
     // Системные методы
@@ -42,6 +42,9 @@ public class Register2_Presenter implements iRegister2.Presenter {
     public void registerUser(iRegister2.RegistrationCallbacks callbacks) {
         checkFields();
 
+        if (formIsValid()) {
+            doRegistration(callbacks);
+        }
     }
 
     // Внутренние методы
@@ -56,6 +59,7 @@ public class Register2_Presenter implements iRegister2.Presenter {
 
         if (TextUtils.isEmpty(name)) {
             view.showNameError(R.string.REGISTER2_cannot_be_empty);
+            formCheckResults.put("email", false);
             return;
         }
 
@@ -70,16 +74,18 @@ public class Register2_Presenter implements iRegister2.Presenter {
             @Override
             public void onExists() {
                 view.showNameError(R.string.REGISTER2_name_already_used);
+                formCheckResults.put("name", false);
             }
 
             @Override
             public void onNotExists() {
-
+                formCheckResults.put("name", true);
             }
 
             @Override
             public void onCheckFail(String errorMsg) {
                 view.showErrorMsg(R.string.REGISTER2_error, errorMsg);
+                formCheckResults.put("name", false);
             }
         });
     }
@@ -89,6 +95,7 @@ public class Register2_Presenter implements iRegister2.Presenter {
 
         if (TextUtils.isEmpty(email)) {
             view.showEmailError(R.string.REGISTER2_cannot_be_empty);
+            formCheckResults.put("email", false);
             return;
         }
 
@@ -96,6 +103,7 @@ public class Register2_Presenter implements iRegister2.Presenter {
         Matcher matcher = pattern.matcher(email);
         if (!matcher.matches()) {
             view.showEmailError(R.string.REGISTER2_incorrect_email);
+            formCheckResults.put("email", false);
             return;
         }
 
@@ -110,16 +118,18 @@ public class Register2_Presenter implements iRegister2.Presenter {
             @Override
             public void onExists() {
                 view.showEmailError(R.string.REGISTER2_email_already_used);
+                formCheckResults.put("email", false);
             }
 
             @Override
             public void onNotExists() {
-
+                formCheckResults.put("email", true);
             }
 
             @Override
             public void onCheckFail(String errorMsg) {
                 view.showErrorMsg(R.string.REGISTER2_error, errorMsg);
+                formCheckResults.put("email", false);
             }
         });
     }
@@ -130,23 +140,45 @@ public class Register2_Presenter implements iRegister2.Presenter {
 
         if (TextUtils.isEmpty(password1)) {
             view.showPassword1Error(R.string.REGISTER2_cannot_be_empty);
+            formCheckResults.put("password", false);
             return;
         }
 
         if (TextUtils.isEmpty(password2)) {
             view.showPassword2Error(R.string.REGISTER2_cannot_be_empty);
+            formCheckResults.put("password", false);
             return;
         }
 
         if (!password1.equals(password2)) {
             view.showPassword1Error(R.string.REGISTER2_passwords_mismatch);
             view.showPassword2Error(R.string.REGISTER2_passwords_mismatch);
+            formCheckResults.put("password", false);
             return;
         }
 
         if (password1.length() < Constants.PASSWORD_MIN_LENGTH) {
             view.showPassword1Error(R.string.REGISTER2_password_too_short);
+            formCheckResults.put("password", false);
             return;
         }
+
+        formCheckResults.put("password", true);
+    }
+
+    private boolean formIsValid() {
+        Set<Map.Entry<String,Boolean>> entrySet = formCheckResults.entrySet();
+        int formSize = entrySet.size();
+        int resultsSize = 0;
+        for (Map.Entry entry : entrySet) {
+            boolean value = (Boolean) entry.getValue();
+            if (value) resultsSize += 1;
+            else resultsSize -= 1;
+        }
+        return (resultsSize == formSize);
+    }
+
+    private void doRegistration(iRegister2.RegistrationCallbacks callbacks) {
+        view.showToast("РЕГИСТРАЦИЯ ПОШЛА");
     }
 }
