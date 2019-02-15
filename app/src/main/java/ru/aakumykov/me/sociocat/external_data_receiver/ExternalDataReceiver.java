22ae632284febfb4d5650e1b19e856dad68fcfbd
Андрей Кумыков <aakumykov@yandex.ru>
2019-02-15 23:38:12 +0700

    Какая-то проблема с ExternalDataReciever.

--- app/src/main/java/ru/aakumykov/me/sociocat/external_data_receiver/ExternalDataReceiver.java
+++ app/src/main/java/ru/aakumykov/me/sociocat/external_data_receiver/ExternalDataReceiver.java
@@ -2,10 +2,15 @@ package ru.aakumykov.me.sociocat.external_data_receiver;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.Menu;
 
 import ru.aakumykov.me.sociocat.BaseView;
+import ru.aakumykov.me.sociocat.Constants;
 import ru.aakumykov.me.sociocat.R;
 import ru.aakumykov.me.sociocat.card_edit.CardEdit_View;
+import ru.aakumykov.me.sociocat.card_show.CardShow_View;
+import ru.aakumykov.me.sociocat.models.Card;
 
 public class ExternalDataReceiver extends BaseView {
 
@@ -36,6 +41,23 @@ public class ExternalDataReceiver extends BaseView {
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        switch (requestCode) {
+            case Constants.CODE_CREATE_CARD:
+                processCardCreationResult(resultCode, data);
+                break;
+            default:
+                super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
 
     private void makeStartDecision() throws Exception {
 
@@ -53,7 +75,27 @@ public class ExternalDataReceiver extends BaseView {
         }
 
         // Пересылаю Intent другой странице
+        intent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
         intent.setClass(this, CardEdit_View.class);
+        startActivityForResult(intent, Constants.CODE_CREATE_CARD);
+    }
+
+    private void processCardCreationResult(int resultCode, @Nullable Intent data) {
+        if (null == data) {
+            throw  new IllegalArgumentException("Intent is null");
+        }
+
+        if (RESULT_CANCELED == resultCode) {
+            finish();
+            return;
+        }
+
+//        data.setClass(this, CardShow_View.class);
+//        startActivity(data);
+
+        Card card = data.getParcelableExtra(Constants.CARD);
+        Intent intent = new Intent(this, CardShow_View.class);
+        intent.putExtra(Constants.CARD, card);
         startActivity(intent);
     }
 }
