f40af8c1577bcdca48e82b229571d450079c9b6d
Андрей Кумыков <aakumykov@yandex.ru>
2019-02-19 18:01:02 +0700

    Эксперимент с горизонтальной прокруткой, в целом, успешен.

--- app/src/main/java/ru/aakumykov/me/sociocat/HorizontalScrollActivity.java
+++ app/src/main/java/ru/aakumykov/me/sociocat/HorizontalScrollActivity.java
@@ -1,7 +1,9 @@
 package ru.aakumykov.me.sociocat;
 
+import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -18,6 +20,7 @@ public class HorizontalScrollActivity extends AppCompatActivity {
     @BindView(R.id.imageView) ImageView imageView;
     @BindView(R.id.messageView) TextView messageView;
     @BindView(R.id.progressBar) ProgressBar progressBar;
+    @BindView(R.id.quoteView) TextView quoteView;
 
 
     @Override
@@ -26,9 +29,25 @@ public class HorizontalScrollActivity extends AppCompatActivity {
         setContentView(R.layout.activity_horizontal_scroll);
         ButterKnife.bind(this);
 
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int width = size.x;
+        int height = size.y;
+
+        quoteView.setText(
+                "ширина: "+width+"\n"+
+                "высота: "+height+"\n"+
+                "пропорция: "+String.valueOf(width*1.0/height*1.0)
+        );
+
         MyUtils.show(progressBar);
 
-        Picasso.get().load("http://www.train-photo.ru/data/media/519/1res.jpg")
+        String wideImage = "http://www.train-photo.ru/data/media/519/1res.jpg";
+        String tallImage1 = "http://loispaigesimenson.com/wp-content/uploads/2015/09/Tall-1.png";
+        String tallImage2 = "http://www.tallheights.com/wp-content/uploads/2016/06/background_purple.jpg";
+
+        Picasso.get().load(wideImage)
                 .into(imageView, new Callback() {
                     @Override
                     public void onSuccess() {
