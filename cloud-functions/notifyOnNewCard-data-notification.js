21cf492db2cbfa1e6fdaf7da517c3d2d0850ab74
Андрей Кумыков <aakumykov@yandex.ru>
2019-03-30 16:49:37 +0700

    Обновил облачную notifyOnNewCard(), имя пользователя карточки.

--- cloud-functions/notifyOnNewCard-data-notification.js
+++ cloud-functions/notifyOnNewCard-data-notification.js
@@ -14,7 +14,8 @@ exports.notifyOnNewCard = functions.database.ref('/cards/{cardId}')
 				data: {
 					card_key: cardData.key,
 					card_title: cardData.title,
-					card_user_id: cardData.userId
+					card_user_id: cardData.userId,
+					card_user_name: cardData.userName
 				}
 			};
 
@@ -31,49 +32,3 @@ exports.notifyOnNewCard = functions.database.ref('/cards/{cardId}')
 		return true;
 	});
 
-
-/*exports.notifyOnNewCardAdvanced = functions.database.ref('/cards/{cardId}').onWrite( (change, context) => {
-
-	if (!change.after.exists()) {
-		return null;
-	}
-
-	let card_user_id = change.after.val().userId;
-	console.log("card_user_id: "+card_user_id);
-
-	let device_id_ref = admin.database().ref("/device_id/");
-
-	device_id_ref.once("value", 
-		(snapshot) => {
-
-		let device_id_map = snapshot.val();
-		console.log("device_id_map:");
-		console.log(device_id_map);
-
-		let target_device_list = {};
-
-		for (device_id in device_id_map) {
-
-			let pieces = device_id_map[device_id].split('__');
-			let device_user_id = pieces[0];
-			let device_last_time = pieces[1];
-			
-			console.log("device_user_id: "+device_user_id);
-			console.log("device_last_time: "+device_last_time);
-			
-			if (device_user_id !== card_user_id) {
-				target_device_list[target_device_list.length] = device_id;
-			}
-
-			console.log("target_device_list:");
-			console.log(target_device_list);
-		}
-
-		}, 
-		(errorObject) => {
-			console.log("The read failed: " + errorObject.code);
-		}
-	);
-
-	return true;
-});*/
