aa634280ffaf3859f8b2fa9b6ab9264d7308e789
Андрей Кумыков <aakumykov@yandex.ru>
2019-05-20 22:54:37 +0700

    fix: инициализация Throbber_ViewHolder

--- app/src/main/java/ru/aakumykov/me/sociocat/card_show/view_holders/Throbber_ViewHolder.java
+++ app/src/main/java/ru/aakumykov/me/sociocat/card_show/view_holders/Throbber_ViewHolder.java
@@ -1,19 +1,24 @@
 package ru.aakumykov.me.sociocat.card_show.view_holders;
 
 import android.view.View;
+import android.widget.TextView;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import ru.aakumykov.me.sociocat.R;
 import ru.aakumykov.me.sociocat.card_show.list_items.Throbber_Item;
 
 public class Throbber_ViewHolder extends Base_ViewHolder
 {
+    @BindView(R.id.throbberMessageView) TextView throbberMessageView;
+
     public Throbber_ViewHolder(View itemView) {
         super(itemView);
         ButterKnife.bind(this, itemView);
     }
 
     public void initialize(Throbber_Item throbberItem) {
-
+        throbberMessageView.setText(throbberItem.getMessage());
     }
 }
 
