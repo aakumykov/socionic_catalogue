4d0b4221a785111f3368c2b820a69d40917b24d9
Андрей Кумыков <aakumykov@yandex.ru>
2019-06-18 12:34:16 +0700

    Порция загрузки карточек - 20.

--- app/src/main/java/ru/aakumykov/me/sociocat/Config.java
+++ app/src/main/java/ru/aakumykov/me/sociocat/Config.java
@@ -24,6 +24,6 @@ public final class Config {
     public static final String YOUTUBE_LONG_LINK_REGEX_1 = "^https?://youtube\\.com/watch\\?v=([\\w-]+)";
     public static final String YOUTUBE_LONG_LINK_REGEX_2 = "^https?://www\\.youtube\\.com/watch\\?v=([\\w-]+)";
 
-    public static final int DEFAULT_CARDS_LOAD_COUNT = 5;
+    public static final int DEFAULT_CARDS_LOAD_COUNT = 20;
     public static final int DEFAULT_COMMENTS_LOAD_COUNT = 5;
 }
