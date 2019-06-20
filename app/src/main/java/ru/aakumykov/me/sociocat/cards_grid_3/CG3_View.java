ac03617c6aa87215f6a35588e743d9a3d05adbca
Андрей Кумыков <aakumykov@yandex.ru>
2019-06-20 17:55:40 +0700

    Перевёл плиточный вид на cg3_activity_constraint.

--- app/src/main/java/ru/aakumykov/me/sociocat/cards_grid_3/CG3_View.java
+++ app/src/main/java/ru/aakumykov/me/sociocat/cards_grid_3/CG3_View.java
@@ -35,7 +35,7 @@ public class CG3_View extends BaseView implements
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.cg3_activity);
+        setContentView(R.layout.cg3_activity_constraint);
         ButterKnife.bind(this);
 
         setPageTitle(R.string.CARDS_GRID_page_title);
