8a438b0ffec8e946803f2a5af087586ba4fa09c7
Андрей Кумыков <aakumykov@yandex.ru>
2019-06-21 12:19:28 +0700

    Что-то намутил с запросом авторизации при создании карточки через быстрые ссылки. Сам не понял, что. Не работает.

--- app/src/main/java/ru/aakumykov/me/sociocat/card_edit/CardEdit_Presenter.java
+++ app/src/main/java/ru/aakumykov/me/sociocat/card_edit/CardEdit_Presenter.java
@@ -5,6 +5,7 @@ import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -80,7 +81,16 @@ public class CardEdit_Presenter implements
             throw new IllegalArgumentException("Intent is NULL");
 
         if (!AuthSingleton.isLoggedIn()) {
-            view.requestLogin(Constants.CODE_EDIT_CARD, intent);
+            String action = intent.getAction();
+            if (null != action) {
+                int requestCode = action.startsWith("CREATE_") ?
+                        Constants.CODE_CREATE_CARD :
+                        Constants.CODE_EDIT_CARD;
+                view.requestLogin(requestCode, intent);
+            }
+            else {
+                view.showErrorMsg(R.string.CARD_EDIT_data_error, "There is no action in Intent");
+            }
             return;
         }
 
