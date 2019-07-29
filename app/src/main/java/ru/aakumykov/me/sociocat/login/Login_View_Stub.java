2a8635932785b8b84c08368bee0617aeb2d73df9
Андрей Кумыков <aakumykov@yandex.ru>
2019-07-29 19:41:15 +0700

    Login_View_Stub()

---
+++ app/src/main/java/ru/aakumykov/me/sociocat/login/Login_View_Stub.java
@@ -0,0 +1,30 @@
+package ru.aakumykov.me.sociocat.login;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+import ru.aakumykov.me.sociocat.BaseView_Stub;
+
+public class Login_View_Stub extends BaseView_Stub implements iLogin.View {
+    @Override
+    public void disableForm() {
+
+    }
+
+    @Override
+    public void enableForm() {
+
+    }
+
+    @Override
+    public void finishLogin(boolean isCancelled, Intent transitIntent, @Nullable Bundle transitArguments) {
+
+    }
+
+    @Override
+    public void notifyToConfirmEmail(String userId) {
+
+    }
+}
