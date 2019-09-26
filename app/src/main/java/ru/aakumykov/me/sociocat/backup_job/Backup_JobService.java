3d9af26e3450a8e5bd1cfc3db0e79225b54e269b
Андрей Кумыков <aakumykov@yandex.ru>
2019-09-27 01:21:10 +0700

    Заготовка Backup_JobService.

---
+++ app/src/main/java/ru/aakumykov/me/sociocat/backup_job/Backup_JobService.java
@@ -0,0 +1,71 @@
+package ru.aakumykov.me.sociocat.backup_job;
+
+import android.app.job.JobInfo;
+import android.app.job.JobParameters;
+import android.app.job.JobScheduler;
+import android.app.job.JobService;
+import android.content.ComponentName;
+import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.concurrent.TimeUnit;
+
+import ru.aakumykov.me.sociocat.R;
+import ru.aakumykov.me.sociocat.utils.MyUtils;
+
+public class Backup_JobService extends JobService {
+
+    private final static String TAG = "Backup_JobService";
+    private final static int sJobId = R.id.backup_job_service_id;
+
+    public static void scheduleJob(Context context)
+    {
+        ComponentName backupJobService = new ComponentName(context, Backup_JobService.class);
+
+        JobInfo.Builder jobBuilder = new JobInfo.Builder(sJobId, backupJobService);
+                        jobBuilder.setPeriodic(TimeUnit.MINUTES.toMillis(1));
+                        jobBuilder.setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY);
+                        jobBuilder.setRequiresDeviceIdle(false);
+                        jobBuilder.setRequiresCharging(false);
+                        jobBuilder.setPersisted(true);
+
+        jobBuilder.setBackoffCriteria(
+                TimeUnit.SECONDS.toMillis(10),
+                JobInfo.BACKOFF_POLICY_LINEAR
+        );
+
+        JobScheduler jobScheduler = (JobScheduler) context.getSystemService(Context.JOB_SCHEDULER_SERVICE);
+
+        int result = jobScheduler.schedule(jobBuilder.build());
+
+        if (JobScheduler.RESULT_SUCCESS == result) {
+            String msg = MyUtils.getString(context, R.string.BACKUP_JOB_SERVICE_job_is_scheduled);
+            Log.d(TAG, msg);
+        }
+        else {
+            String msg = MyUtils.getString(context, R.string.BACKUP_JOB_SERVICE_error_scheduling_job);
+            Log.e(TAG, msg);
+        }
+
+    }
+
+    public static void unscheduleJob(Context context)
+    {
+        JobScheduler jobScheduler = (JobScheduler) context.getSystemService(Context.JOB_SCHEDULER_SERVICE);
+        jobScheduler.cancel(sJobId);
+    }
+
+
+    @Override
+    public boolean onStartJob(JobParameters params) {
+        Log.d(TAG, "onStartJob()");
+        return false;
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters params) {
+        Log.d(TAG, "onStopJob()");
+        return false;
+    }
+}
