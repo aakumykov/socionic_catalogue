618a07a0b1538efd47084d754f390fdafc0aa27c
Андрей Кумыков <aakumykov@yandex.ru>
2019-09-28 21:33:46 +0700

    Запилил в BackupService методы Firebase2Dropbox.

--- app/src/main/java/ru/aakumykov/me/sociocat/backup_job/BackupService.java
+++ app/src/main/java/ru/aakumykov/me/sociocat/backup_job/BackupService.java
@@ -5,87 +5,34 @@ import android.content.Intent;
 import android.os.IBinder;
 import android.os.Parcel;
 import android.os.Parcelable;
+import android.text.TextUtils;
 import android.util.Log;
+import android.util.Pair;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import ru.aakumykov.me.sociocat.R;
+import ru.aakumykov.me.sociocat.models.Card;
+import ru.aakumykov.me.sociocat.models.Comment;
+import ru.aakumykov.me.sociocat.models.Tag;
+import ru.aakumykov.me.sociocat.models.User;
+import ru.aakumykov.me.sociocat.utils.MyUtils;
+
 public class BackupService extends Service {
-
-    public static final String BROADCAST_BACKUP_SERVICE = "ru.aakumykov.me.sociocat.BROADCAST_BACKUP_SERVICE";
-
-    public static final String EXTRA_BACKUP_INFO = "EXTRA_BACKUP_INFO";
-
-    public final static String BACKUP_STATUS_START =   "BACKUP_STATUS_START";
-    public final static String BACKUP_STATUS_RUNNING = "BACKUP_STATUS_RUNNING";
-    public final static String BACKUP_STATUS_FINISH =  "BACKUP_STATUS_FINISH";
-
-    public final static String BACKUP_RESULT_SUCCESS = "BACKUP_RESULT_SUCCESS";
-    public final static String BACKUP_RESULT_ERROR =   "BACKUP_RESULT_ERROR";
-
-    private final static String TAG = "BackupService";
-
-
-    // Системные методы
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        Log.d(TAG, "onCreate()");
-    }
-
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        super.onStartCommand(intent, flags, startId);
-        Log.d(TAG, "onStartCommand()");
-
-        String name = "Служба резервного копирования";
-
-        sendBroadcast(new BackupInfo(name, BACKUP_STATUS_START));
-
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                int max = 5;
-
-                for (int i=0; i<max; i++) {
-                    try {
-                        TimeUnit.SECONDS.sleep(1);
-                    }
-                    catch (InterruptedException e) {}
-
-                    sendBroadcast(new BackupInfo(name, BACKUP_STATUS_RUNNING).setProgress(i).setProgressMax(max));
-                }
-
-                stopSelf(startId);
-
-                sendBroadcast(new BackupInfo(name, BACKUP_STATUS_FINISH));
-            }
-        }).start();
-
-        return START_NOT_STICKY;
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        Log.d(TAG, "onDestroy()");
-    }
-
-    @Nullable @Override
-    public IBinder onBind(Intent intent) {
-        return null;
-    }
-
-
-    // Внутренние методы
-    private void sendBroadcast(BackupInfo backupInfo) {
-        Intent intent = new Intent(BROADCAST_BACKUP_SERVICE);
-        intent.putExtra(EXTRA_BACKUP_INFO, backupInfo);
-        sendBroadcast(intent);
-    }
-
 
     // Вложенные классы
     public static class BackupInfo implements Parcelable {
@@ -93,6 +40,7 @@ public class BackupService extends Service {
         private String name;
         private String backupStatus;
         private String backupResult;
+        private String message;
         private long progress;
         private int progressMax;
 
@@ -114,6 +62,9 @@ public class BackupService extends Service {
         public String getBackupResult() {
             return backupResult;
         }
+        public String getMessage() {
+            return this.message;
+        }
         public long getProgress() {
             return progress;
         }
@@ -131,6 +82,10 @@ public class BackupService extends Service {
         }
         public BackupInfo setBackupResult(String backupResult) {
             this.backupResult = backupResult;
+            return this;
+        }
+        public BackupInfo setMessage(String message) {
+            this.message = message;
             return this;
         }
         public BackupInfo setProgress(long progress) {
@@ -151,7 +106,7 @@ public class BackupService extends Service {
                     ", backupResult: " + getBackupResult() +
                     ", progress: " + getProgress() +
                     ", progressMax: " + getProgressMax() +
-            "}";
+                    "}";
         }
 
         // Конверт
@@ -188,4 +143,325 @@ public class BackupService extends Service {
         };
         // Конверт
     }
+    private static class CollectionPair {
+        private String name;
+        private Class itemClass;
+
+        public CollectionPair(String name, Class itemClass) {
+            this.name = name;
+            this.itemClass = itemClass;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public Class getItemClass() {
+            return itemClass;
+        }
+    }
+    private static class CollectionPool {
+
+        public CollectionPool(CollectionPair... pairs) {
+            for (CollectionPair pair : pairs)
+                this.push(pair);
+        }
+
+        private List<CollectionPair> list = new ArrayList<>();
+
+        public CollectionPair pop() {
+            int index = list.size()-1;
+            if (index >= 0) {
+                CollectionPair collectionPair = list.get(index);
+                list.remove(index);
+                return collectionPair;
+            }
+            else {
+                return null;
+            }
+        }
+
+        public void push(CollectionPair collectionPair) {
+            list.add(collectionPair);
+        }
+    }
+
+
+    public static final String BROADCAST_BACKUP_SERVICE = "ru.aakumykov.me.sociocat.BROADCAST_BACKUP_SERVICE";
+    public static final String EXTRA_BACKUP_INFO = "EXTRA_BACKUP_INFO";
+    public final static String BACKUP_STATUS_START =   "BACKUP_STATUS_START";
+    public final static String BACKUP_STATUS_RUNNING = "BACKUP_STATUS_RUNNING";
+    public final static String BACKUP_STATUS_FINISH =  "BACKUP_STATUS_FINISH";
+    public final static String BACKUP_RESULT_SUCCESS = "BACKUP_RESULT_SUCCESS";
+    public final static String BACKUP_RESULT_ERROR =   "BACKUP_RESULT_ERROR";
+    private final static String TAG = "BackupService";
+
+    private String dropboxAccessToken;
+    private DropboxBackuper dropboxBackuper;
+    private CollectionPool collectionPool = new CollectionPool(
+            new CollectionPair("admins", User.class),
+            new CollectionPair("user", User.class),
+            new CollectionPair("cards", Card.class),
+            new CollectionPair("tags", Tag.class),
+            new CollectionPair("comments", Comment.class)
+    );
+
+    private List<String> backupSuccessList = new ArrayList<>();
+    private List<String> backupErrorsList = new ArrayList<>();
+
+
+    // Системные методы
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(TAG, "onCreate()");
+        dropboxAccessToken = getResources().getString(R.string.DROPBOX_ACCESS_TOKEN);
+        dropboxBackuper = new DropboxBackuper(dropboxAccessToken);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        super.onStartCommand(intent, flags, startId);
+        Log.d(TAG, "onStartCommand()");
+
+/*
+        String name = "Служба резервного копирования";
+
+        sendBroadcast(new BackupInfo(name, BACKUP_STATUS_START));
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int max = 5;
+
+                for (int i=0; i<max; i++) {
+                    try {
+                        TimeUnit.SECONDS.sleep(1);
+                    }
+                    catch (InterruptedException e) {}
+
+                    sendBroadcast(new BackupInfo(name, BACKUP_STATUS_RUNNING).setProgress(i).setProgressMax(max));
+                }
+
+                stopSelf(startId);
+
+                sendBroadcast(new BackupInfo(name, BACKUP_STATUS_FINISH));
+            }
+        }).start();
+*/
+
+        startBackup();
+
+        return START_NOT_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy()");
+    }
+
+    @Nullable @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+
+    // Главные внутренние методы
+    private void startBackup() {
+        String dirName = "qwerty";
+        String initialDirName = MyUtils.quoteString(this, dirName);
+        Log.d(TAG, "startBackup(), initialDirName: "+initialDirName);
+
+        sendBroadcast(new BackupInfo(TAG, BACKUP_STATUS_START));
+
+        dropboxBackuper.createDir(dirName, true, new DropboxBackuper.iCreateDirCallbacks() {
+            @Override
+            public void onCreateDirSuccess(String createdDirName) {
+                String successMsg = "Создан каталог " + MyUtils.quoteString(BackupService.this, createdDirName);
+                backupSuccessList.add(successMsg);
+                Log.d(TAG, successMsg);
+
+                performCollectionsBackup(createdDirName);
+            }
+
+            @Override
+            public void onCreateDirFail(String errorMsg) {
+                backupErrorsList.add(errorMsg);
+                sendBroadcast(
+                        new BackupInfo(TAG, BACKUP_STATUS_FINISH)
+                        .setBackupResult(BACKUP_RESULT_ERROR)
+                        .setMessage(errorMsg)
+                );
+            }
+        });
+    }
+
+    private void performCollectionsBackup(String targetDirName) {
+
+        CollectionPair collectionPair = collectionPool.pop();
+
+        if (null != collectionPair) {
+            String collectionName = collectionPair.name;
+            Class itemClass = collectionPair.itemClass;
+
+            String msg = "Загрузка коллекции "+collectionPair.getName();
+            Log.d(TAG, msg);
+
+            loadCollection(collectionName, itemClass, new iLoadCollectionCallbacks() {
+                @Override
+                public void onLoadCollectionSuccess(List<Object> itemsList, List<String> errorsList) {
+                    String jsonData = listOfObjects2JSON(itemsList);
+
+                    String msg = "Сохранение коллекции "+collectionName;
+                    Log.d(TAG, msg);
+
+                    dropboxBackuper.backupString(
+                            targetDirName,
+                            collectionName,
+                            "json",
+                            jsonData,
+                            true,
+                            new DropboxBackuper.iBackupStringCallbacks() {
+                                @Override
+                                public void onBackupSuccess(DropboxBackuper.BackupItemInfo backupItemInfo) {
+                                    backupSuccessList.add("Коллекция "+collectionName+" обработана");
+                                    String msg = "Коллекция "+collectionName+" сохранена";
+                                    Log.d(TAG, msg);
+
+                                    sendBroadcast(
+                                            new BackupInfo(TAG, BACKUP_STATUS_RUNNING)
+                                                    .setBackupResult(BACKUP_RESULT_SUCCESS)
+                                                    .setMessage(msg)
+                                    );
+
+                                    performCollectionsBackup(targetDirName);
+                                }
+
+                                @Override
+                                public void onBackupFail(String errorMsg) {
+                                    String msg = "Ошибка обработки "+collectionName+": "+errorMsg;
+                                    backupErrorsList.add(msg);
+                                    Log.e(TAG, msg);
+
+                                    sendBroadcast(
+                                            new BackupInfo(TAG, BACKUP_STATUS_RUNNING)
+                                                    .setBackupResult(BACKUP_RESULT_ERROR)
+                                                    .setMessage(msg)
+                                    );
+
+                                    performCollectionsBackup(targetDirName);
+                                }
+                            }
+                    );
+                }
+
+                @Override
+                public void onLoadCollectionError(String errorMsg) {
+                    String msg = "Ошибка получения коллекции "+collectionName;
+                    Log.e(TAG, msg);
+
+                    sendBroadcast(
+                            new BackupInfo(TAG, BACKUP_STATUS_RUNNING)
+                                    .setBackupResult(BACKUP_RESULT_ERROR)
+                                    .setMessage(msg)
+                    );
+
+                    performCollectionsBackup(targetDirName);
+                }
+            });
+        }
+        else {
+            Log.d(TAG, "Все коллекции обработаны");
+            sendBroadcast(
+                    new BackupInfo(TAG, BACKUP_STATUS_FINISH)
+                    .setBackupResult(BACKUP_RESULT_SUCCESS)
+                    .setMessage("Все коллекции обработаны")
+            );
+        }
+    }
+
+    private void loadCollection(String collectionName, Class itemClass, iLoadCollectionCallbacks callbacks) {
+        FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+        CollectionReference collectionReference = firebaseFirestore.collection(collectionName);
+
+        collectionReference.get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        Pair<List<Object>, List<String>> resultPair = extractCollectionObjects(queryDocumentSnapshots, itemClass);
+
+                        List<Object> itemsList = resultPair.first;
+                        List<String> errorsList = resultPair.second;
+
+                        callbacks.onLoadCollectionSuccess(itemsList, errorsList);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        String errorMsg = e.getMessage();
+                        Log.e(TAG, errorMsg);
+                        e.printStackTrace();
+
+                        callbacks.onLoadCollectionError(errorMsg);
+                    }
+                });
+    }
+
+    private Pair<List<Object>, List<String>> extractCollectionObjects(QuerySnapshot queryDocumentSnapshots, Class itemClass) {
+
+        List<String> errorsList = new ArrayList<>();
+        List<Object> itemsList = new ArrayList<>();
+
+        for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots)
+        {
+            try {
+                Object item = documentSnapshot.toObject(itemClass);
+                itemsList.add(item);
+            }
+            catch (Exception e) {
+                errorsList.add(Arrays.toString(e.getStackTrace()));
+            }
+        }
+
+        return new Pair<>(itemsList, errorsList);
+    }
+
+    private String listOfObjects2JSON(List<Object> itemsList) {
+
+        List<String> jsonList = new ArrayList<>();
+        List<String> errorsList = new ArrayList<>();
+
+        Gson gson = new Gson();
+
+        for (Object item : itemsList) {
+            try {
+                jsonList.add(gson.toJson(item));
+            }
+            catch (Exception e) {
+                errorsList.add(Arrays.toString(e.getStackTrace()));
+            }
+        }
+
+        if (errorsList.size() > 0) {
+            Log.e(TAG, errorsList.toString());
+        }
+
+        return "[\n" + TextUtils.join(",\n", jsonList) + "\n]";
+    }
+
+    private interface iLoadCollectionCallbacks {
+        void onLoadCollectionSuccess(List<Object> itemsList, List<String> errorsList);
+        void onLoadCollectionError(String errorMsg);
+    }
+
+
+    // Служебные внутренние методы
+    private void sendBroadcast(BackupInfo backupInfo) {
+        Intent intent = new Intent(BROADCAST_BACKUP_SERVICE);
+        intent.putExtra(EXTRA_BACKUP_INFO, backupInfo);
+        sendBroadcast(intent);
+    }
+
 }
