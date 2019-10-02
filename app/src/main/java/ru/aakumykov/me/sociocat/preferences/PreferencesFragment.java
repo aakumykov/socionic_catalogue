09e0183aba7ac7ac08c3a4d9dd8553ba28825e06
Андрей Кумыков <aakumykov@yandex.ru>
2019-10-03 00:08:20 +0700

    Планирование/разпланирование задачи при переключении настройки.

--- app/src/main/java/ru/aakumykov/me/sociocat/preferences/PreferencesFragment.java
+++ app/src/main/java/ru/aakumykov/me/sociocat/preferences/PreferencesFragment.java
@@ -1,5 +1,6 @@
 package ru.aakumykov.me.sociocat.preferences;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
@@ -7,6 +8,8 @@ import android.preference.PreferenceFragment;
 import androidx.annotation.Nullable;
 
 import ru.aakumykov.me.sociocat.R;
+import ru.aakumykov.me.sociocat.backup_job.BackupService;
+import ru.aakumykov.me.sociocat.backup_job.Backup_JobService;
 
 public class PreferencesFragment extends PreferenceFragment implements
         SharedPreferences.OnSharedPreferenceChangeListener
@@ -32,6 +35,7 @@ public class PreferencesFragment extends PreferenceFragment implements
         sharedPreferences.unregisterOnSharedPreferenceChangeListener(this);
     }
 
+
     // TODO: что со статусом авторизации?
 
     @Override
@@ -39,5 +43,14 @@ public class PreferencesFragment extends PreferenceFragment implements
 
         PreferencesProcessor.processPreferenceKey(getActivity(), sharedPreferences, key);
 
+        switch (key) {
+            case "perform_database_backup":
+                boolean enabled = sharedPreferences.getBoolean(key, false);
+                if (enabled)
+                    Backup_JobService.scheduleJob(getActivity());
+                else
+                    Backup_JobService.unscheduleJob(getActivity());
+                break;
+        }
     }
 }
