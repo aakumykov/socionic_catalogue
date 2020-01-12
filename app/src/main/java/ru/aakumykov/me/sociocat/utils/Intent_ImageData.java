785db4200b17605697a7afb885d375a6e20eb842
Андрей Кумыков <aakumykov@yandex.ru>
2020-01-12 16:32:45 +0700

    Новые служебные классы для извлечения сведений об изображении и об Uri из Intent-ов.

---
+++ app/src/main/java/ru/aakumykov/me/sociocat/utils/Intent_ImageData.java
@@ -0,0 +1,54 @@
+package ru.aakumykov.me.sociocat.utils;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+
+import androidx.annotation.Nullable;
+
+public class Intent_ImageData {
+
+    private static final String TAG = "Intent_ImageData";
+
+    private ImageType imageType = null;
+    private Uri imageURI = null;
+    private String error;
+
+
+    public Intent_ImageData(Context context, @Nullable Intent intent) {
+
+        try {
+            imageURI = Intent_URIExtractor.extractUriFromIntent(intent);
+        }
+        catch (Intent_URIExtractor.IntentURIExtractor_Exception e) {
+            this.error = e.getMessage();
+            return;
+        }
+
+
+        try {
+            this.imageType = URI_ImageTypeExtractor.extractImageTypeFromURI(context, imageURI);
+        }
+        catch (URI_ImageTypeExtractor.URIImageTypeExtractor_Exception e) {
+            this.error = e.getMessage();
+            return;
+        }
+    }
+
+
+    public ImageType getImageType() {
+        return imageType;
+    }
+
+    public Uri getImageURI() {
+        return imageURI;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public boolean noError() {
+        return null == error;
+    }
+}
