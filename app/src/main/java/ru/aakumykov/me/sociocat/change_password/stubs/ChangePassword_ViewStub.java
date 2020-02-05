e3112a62c93e49a27e7a50d81c92b9303ae335b8
Andrey Kumykov <aakumykov@yandex.ru>
2020-02-05 10:37:38 +0700

    Заготовка страницы изменения пароля.

---
+++ app/src/main/java/ru/aakumykov/me/sociocat/change_password/stubs/ChangePassword_ViewStub.java
@@ -0,0 +1,26 @@
+package ru.aakumykov.me.sociocat.change_password.stubs;
+
+import android.annotation.SuppressLint;
+
+import ru.aakumykov.me.sociocat.BaseView_Stub;
+import ru.aakumykov.me.sociocat.change_password.iChangePassword;
+import ru.aakumykov.me.sociocat.change_password.models.Item;
+
+@SuppressLint("Registered")
+public class ChangePassword_ViewStub extends BaseView_Stub implements iChangePassword.iView {
+
+    @Override
+    public void displayItem(Item item) {
+
+    }
+
+    @Override
+    public void hideRefreshThrobber() {
+
+    }
+
+    @Override
+    public void goCardsGrid() {
+
+    }
+}
