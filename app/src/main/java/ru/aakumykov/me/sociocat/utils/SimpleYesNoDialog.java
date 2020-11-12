b54751b3c5767d42a5390b2bd0dddca16c513c4c
Andrey Kumykov <aakumykov@yandex.ru>
2020-11-12 21:13:44 +0700

    Проверка обязательных аргументов в SimpleYesNoDialog.

--- app/src/main/java/ru/aakumykov/me/sociocat/utils/SimpleYesNoDialog.java
+++ app/src/main/java/ru/aakumykov/me/sociocat/utils/SimpleYesNoDialog.java
@@ -2,6 +2,7 @@ package ru.aakumykov.me.sociocat.utils;
 
 import android.content.Context;
 import android.content.DialogInterface;
+import android.util.Pair;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,17 +17,19 @@ public class SimpleYesNoDialog {
     Иначе не определяется тема и вываливается исключение. */
     public static void show(
             @NonNull Context context,
-            @Nullable Integer titleId,
-            @Nullable Integer messageId,
+            @Nullable Object title,
+            @Nullable Object message,
             Callbacks callbacks
     ) {
+        Pair<String,String> titleAndMessage = checkTitleAndMessage(context, title, message);
+
         AlertDialog.Builder builder = new AlertDialog.Builder(context);
 
-        if (null != titleId)
-            builder.setTitle(titleId);
+        if (null != titleAndMessage.first)
+            builder.setTitle(titleAndMessage.first);
 
-        if (null != messageId)
-            builder.setMessage(messageId);
+        if (null != titleAndMessage.second)
+            builder.setMessage(titleAndMessage.second);
 
         builder.setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
             @Override
@@ -52,6 +55,28 @@ public class SimpleYesNoDialog {
         AlertDialog alertDialog = builder.create();
 
         alertDialog.show();
+    }
+
+    private static Pair<String,String> checkTitleAndMessage(@NonNull Context context, @Nullable Object title, @Nullable Object message) {
+
+        String titleText = getStringFromObject(context, title);
+        String messageText = getStringFromObject(context, message);
+
+        if (null == messageText && null == titleText)
+            throw new RuntimeException("You must supply title or message, or both");
+
+        return new Pair<>(titleText, messageText);
+    }
+
+    private static String getStringFromObject(@NonNull Context context, @Nullable Object title) {
+        String text = null;
+
+        if (title instanceof Integer)
+            text = MyUtils.getString(context, (int) title);
+        else if (title instanceof String)
+            text = (String) title;
+
+        return text;
     }
 
 
