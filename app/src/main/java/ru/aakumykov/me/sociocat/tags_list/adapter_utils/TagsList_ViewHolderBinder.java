685cc5e525c8ccb05e03cf0f17aef13b0a856ba9
Andrey Kumykov <aakumykov@yandex.ru>
2020-11-16 11:27:42 +0700

    Revert "Пробная прокрутка на списке меток работает."
    This reverts commit c099a5e0

--- app/src/main/java/ru/aakumykov/me/sociocat/tags_list/adapter_utils/TagsList_ViewHolderBinder.java
+++ app/src/main/java/ru/aakumykov/me/sociocat/tags_list/adapter_utils/TagsList_ViewHolderBinder.java
@@ -13,7 +13,7 @@ public class TagsList_ViewHolderBinder extends BasicMVP_ViewHolderBinder {
     public void bindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position, BasicMVP_ListItem listItem) {
         if (holder instanceof Tag_ViewHolder) {
             Tag_ViewHolder tagViewHolder = (Tag_ViewHolder) holder;
-            tagViewHolder.fillWithData(listItem, position);
+            tagViewHolder.fillWithData(listItem);
         }
         else
             super.bindViewHolder(holder, position, listItem);
