838b2fe60d5c0e481728eff460791d3e5cc7b737
Andrey Kumykov <aakumykov@yandex.ru>
2020-11-19 13:07:59 +0700

    AllSelectedViewState наследует SelectionViewState.

--- app/src/main/java/ru/aakumykov/me/sociocat/b_basic_mvp_components2/view_states/AllSelectedViewState.java
+++ app/src/main/java/ru/aakumykov/me/sociocat/b_basic_mvp_components2/view_states/AllSelectedViewState.java
@@ -1,17 +1,9 @@
 package ru.aakumykov.me.sociocat.b_basic_mvp_components2.view_states;
 
 
-import ru.aakumykov.me.sociocat.b_basic_mvp_components2.interfaces.iBasicViewState;
-
-public class AllSelectedViewState implements iBasicViewState {
-
-    private int mSelectedItemsCount = -1;
+public class AllSelectedViewState extends SelectionViewState {
 
     public AllSelectedViewState(int selectedItemsCount) {
-        mSelectedItemsCount = selectedItemsCount;
-    }
-
-    public int getSelectedItemsCount() {
-        return mSelectedItemsCount;
+        super(selectedItemsCount);
     }
 }
