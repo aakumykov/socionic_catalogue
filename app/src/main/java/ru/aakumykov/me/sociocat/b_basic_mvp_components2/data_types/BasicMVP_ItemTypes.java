6c50f23a140adb714c8e152baef59a790f9e7328
Andrey Kumykov <aakumykov@yandex.ru>
2020-11-20 21:33:48 +0700

    Переделал TagsList_ViewHolderCreator на новый лад.

--- app/src/main/java/ru/aakumykov/me/sociocat/b_basic_mvp_components2/data_types/BasicMVP_ItemTypes.java
+++ app/src/main/java/ru/aakumykov/me/sociocat/b_basic_mvp_components2/data_types/BasicMVP_ItemTypes.java
@@ -5,8 +5,9 @@ package ru.aakumykov.me.sociocat.b_basic_mvp_components2.data_types;
 public final class BasicMVP_ItemTypes {
 
     public final static int DATA_ITEM = 1;
+
     public final static int LOADMORE_ITEM = 2;
+
     public final static int THROBBER_ITEM = 3;
 
-    public static final int TAG_ITEM = 30;
 }
--- app/src/main/java/ru/aakumykov/me/sociocat/tags_list/adapter_utils/TagsList_ViewHolderCreator.java
+++ app/src/main/java/ru/aakumykov/me/sociocat/tags_list/adapter_utils/TagsList_ViewHolderCreator.java
@@ -1,22 +1,11 @@
 package ru.aakumykov.me.sociocat.tags_list.adapter_utils;
 
-import android.view.View;
 import android.view.ViewGroup;
-
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
 
 import ru.aakumykov.me.sociocat.R;
 import ru.aakumykov.me.sociocat.b_basic_mvp_components2.adapter_utils.BasicMVP_ViewHolderCreator;
-import ru.aakumykov.me.sociocat.b_basic_mvp_components2.data_types.BasicMVP_ItemTypes;
-import ru.aakumykov.me.sociocat.b_basic_mvp_components2.exceptions.UnknownViewModeException;
 import ru.aakumykov.me.sociocat.b_basic_mvp_components2.view_holders.BasicMVP_DataViewHolder;
-import ru.aakumykov.me.sociocat.b_basic_mvp_components2.view_modes.BasicViewMode;
-import ru.aakumykov.me.sociocat.b_basic_mvp_components2.view_modes.FeedViewMode;
-import ru.aakumykov.me.sociocat.b_basic_mvp_components2.view_modes.GridViewMode;
-import ru.aakumykov.me.sociocat.b_basic_mvp_components2.view_modes.ListViewMode;
 import ru.aakumykov.me.sociocat.tags_list.interfaces.iTagsList_ItemClickListener;
-import ru.aakumykov.me.sociocat.tags_list.view_holders.TagViewHolder;
 import ru.aakumykov.me.sociocat.tags_list.view_holders.TagViewHolder_Feed;
 import ru.aakumykov.me.sociocat.tags_list.view_holders.TagViewHolder_Grid;
 import ru.aakumykov.me.sociocat.tags_list.view_holders.TagViewHolder_List;
@@ -27,66 +16,19 @@ public class TagsList_ViewHolderCreator extends BasicMVP_ViewHolderCreator {
         super(itemClickListener);
     }
 
+
     @Override
     public BasicMVP_DataViewHolder createViewHolder4listMode(ViewGroup parent) {
-        return null;
+        return new TagViewHolder_List(inflateItemView(parent, R.layout.tags_item_list));
     }
 
     @Override
     public BasicMVP_DataViewHolder createViewHolder4feedMode(ViewGroup parent) {
-        return null;
+        return new TagViewHolder_Feed(inflateItemView(parent, R.layout.tags_item_feed));
     }
 
     @Override
     public BasicMVP_DataViewHolder createViewHolder4gridMode(ViewGroup parent) {
-        return null;
+        return new TagViewHolder_Grid(inflateItemView(parent, R.layout.tags_item_grid));
     }
-
-    @Override
-    public RecyclerView.ViewHolder createViewHolder(@NonNull ViewGroup parent, int viewType, BasicViewMode viewMode) {
-        if (BasicMVP_ItemTypes.TAG_ITEM == viewType) {
-            return createTagViewHolder(parent, viewMode);
-        }
-        return super.createViewHolder(parent, viewType, viewMode);
-    }
-
-    private TagViewHolder createTagViewHolder(ViewGroup parent, BasicViewMode viewMode) {
-
-        TagViewHolder tagViewHolder = prepareTagViewHolder(parent, viewMode);
-
-        tagViewHolder.setItemClickListener(mItemClickListener);
-
-        return tagViewHolder;
-    }
-
-    private TagViewHolder prepareTagViewHolder(ViewGroup parent, BasicViewMode viewMode) {
-        int layoutId = getLayoutForViewMode(viewMode);
-        View itemView = inflateItemView(parent, layoutId);
-
-        TagViewHolder tagViewHolder;
-
-        if (viewMode instanceof ListViewMode)
-            tagViewHolder = new TagViewHolder_List(itemView);
-        else if (viewMode instanceof GridViewMode)
-            tagViewHolder = new TagViewHolder_Grid(itemView);
-        else if (viewMode instanceof FeedViewMode)
-            tagViewHolder = new TagViewHolder_Feed(itemView);
-        else
-            throw new UnknownViewModeException(viewMode);
-
-        return tagViewHolder;
-    }
-
-    private int getLayoutForViewMode(BasicViewMode viewMode) {
-        if (viewMode instanceof ListViewMode)
-            return R.layout.tags_item_list;
-        else if (viewMode instanceof GridViewMode)
-            return R.layout.tags_item_grid;
-        else if (viewMode instanceof FeedViewMode)
-            return R.layout.tags_item_feed;
-        else
-            throw new UnknownViewModeException(viewMode);
-    }
-
-
 }
