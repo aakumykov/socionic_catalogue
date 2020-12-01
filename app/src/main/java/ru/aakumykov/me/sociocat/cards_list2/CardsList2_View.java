60ba5e95441e550dcc70591549707b0b8d0f8dc6
Андрей Кумыков <aakumykov@yandex.ru>
2020-12-01 13:27:06 +0700

    Вернул пункт меню "Профиль" в список карточек.

--- app/src/main/java/ru/aakumykov/me/sociocat/cards_list2/CardsList2_View.java
+++ app/src/main/java/ru/aakumykov/me/sociocat/cards_list2/CardsList2_View.java
@@ -119,6 +119,8 @@ public class CardsList2_View extends BasicMVPList_View implements iCardsList2_Vi
         inflateMenu(R.menu.change_view_mode);
         inflateMenu(R.menu.tags);
 
+        addProfileMenuItem();
+
         addSortByNameMenu();
         addSortByCommentsMenu();
         addSortByRatingMenu();
