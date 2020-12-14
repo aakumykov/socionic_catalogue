ee8e6a020381eae508ac382e709b69d0f7412e3e
Андрей Кумыков <aakumykov@yandex.ru>
2020-12-14 17:04:12 +0700

    Отображается реальная картинка карточки в режиме сетки.

--- app/src/main/java/ru/aakumykov/me/sociocat/b_cards_list/view_holders/CardViewHolder_Grid.java
+++ app/src/main/java/ru/aakumykov/me/sociocat/b_cards_list/view_holders/CardViewHolder_Grid.java
@@ -1,10 +1,21 @@
 package ru.aakumykov.me.sociocat.b_cards_list.view_holders;
 
+import android.animation.AnimatorSet;
+import android.graphics.drawable.Drawable;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.CustomTarget;
+import com.bumptech.glide.request.target.Target;
+import com.bumptech.glide.request.transition.Transition;
 
 import butterknife.BindView;
 import ru.aakumykov.me.sociocat.AppConfig;
@@ -12,6 +23,7 @@ import ru.aakumykov.me.sociocat.R;
 import ru.aakumykov.me.sociocat.a_basic_mvp_list_components.list_items.BasicMVPList_ListItem;
 import ru.aakumykov.me.sociocat.a_basic_mvp_list_components.utils.ViewUtils;
 import ru.aakumykov.me.sociocat.models.Card;
+import ru.aakumykov.me.sociocat.utils.AnimationUtils;
 
 public class CardViewHolder_Grid extends CardViewHolder {
 
@@ -79,8 +91,55 @@ public class CardViewHolder_Grid extends CardViewHolder {
     }
 
     private void displayImageCard(@NonNull Card card) {
-        imageView.setImageResource(R.drawable.ic_card_type_image_list_mode);
+        /*imageView.setImageResource(R.drawable.ic_card_type_image_list_mode);
+        ViewUtils.show(imageView);*/
+
+        imageView.setImageResource(R.drawable.ic_image_placeholder_smaller);
         ViewUtils.show(imageView);
+        AnimatorSet animatorSet = AnimationUtils.animateFadeInOut(imageView);
+
+        Glide.with(imageView.getContext())
+                .load(card.getImageURL())
+                .listener(new RequestListener<Drawable>() {
+                    @Override
+                    public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                        if (card.isImageCard()) {
+//                            hideQuote();
+//                            hideVideo();
+//                            hideAudio();
+
+                            imageView.setImageDrawable(resource);
+                            AnimationUtils.revealFromCurrentAlphaState(imageView, animatorSet);
+                        }
+//                        else
+//                            hideImage();
+
+                        return true;
+                    }
+
+                    @Override
+                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                        if (card.isImageCard()) {
+                            AnimationUtils.revealFromCurrentAlphaState(imageView, animatorSet);
+                            imageView.setImageResource(R.drawable.ic_image_error);
+                        }
+                        else {
+//                            hideImage();
+                        }
+                        return true;
+                    }
+                })
+                .into(new CustomTarget<Drawable>() {
+                    @Override
+                    public void onResourceReady(@NonNull Drawable resource, @Nullable Transition<? super Drawable> transition) {
+
+                    }
+
+                    @Override
+                    public void onLoadCleared(@Nullable Drawable placeholder) {
+                        AnimationUtils.revealFromCurrentAlphaState(imageView, animatorSet);
+                    }
+                });
     }
 
     private void displayTextCard(@NonNull Card card) {
