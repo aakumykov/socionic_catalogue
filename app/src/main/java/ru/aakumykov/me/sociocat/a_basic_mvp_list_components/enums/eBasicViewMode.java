0f1f74ee57d731fcf1bd003970ab872900fad340
Андрей Кумыков <aakumykov@yandex.ru>
2020-12-19 11:49:33 +0700

    Удалил перечисление eBasicViewMode.

--- app/src/main/java/ru/aakumykov/me/sociocat/a_basic_mvp_list_components/enums/eBasicViewMode.java
+++
@@ -1,9 +0,0 @@
-package ru.aakumykov.me.sociocat.a_basic_mvp_list_components.enums;
-
-import ru.aakumykov.me.sociocat.a_basic_mvp_list_components.interfaces.iViewMode;
-
-public enum eBasicViewMode implements iViewMode {
-    LIST,
-    GRID,
-    FEED
-}
