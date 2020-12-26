efe56045cebdb3470c62a4fcd9d9bf05b7d72d9a
Андрей Кумыков <aakumykov@yandex.ru>
2020-12-26 10:30:05 +0700

    Упрощённо программно создаю два переключателя в настройках.

--- app/src/main/java/ru/aakumykov/me/sociocat/preferences2/Preferences2_Fragment.java
+++ app/src/main/java/ru/aakumykov/me/sociocat/preferences2/Preferences2_Fragment.java
@@ -5,47 +5,79 @@ import android.os.Bundle;
 
 import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceFragmentCompat;
-import androidx.preference.PreferenceManager;
 import androidx.preference.PreferenceScreen;
 import androidx.preference.SwitchPreference;
 
-import ru.aakumykov.me.sociocat.Constants;
-import ru.aakumykov.me.sociocat.R;
 import ru.aakumykov.me.sociocat.singletons.AuthSingleton;
 
 public class Preferences2_Fragment
         extends PreferenceFragmentCompat
         implements SharedPreferences.OnSharedPreferenceChangeListener
 {
+    private final static String PER_USER_PREFS_KEY_DELIMITER = "__";
+
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
 
         String currentUserId = AuthSingleton.currentUserId();
-        if (null != currentUserId) {
-            String cardsNotificationsPrefKey = Constants.PREFERENCE_KEY_notify_about_new_cards + "__" + currentUserId;
+        if (null == currentUserId)
+            return;
 
-            PreferenceManager preferenceManager = getPreferenceManager();
-            PreferenceScreen preferenceScreen = preferenceManager.createPreferenceScreen(getContext());
+        PreferenceScreen preferenceScreen = getPreferenceManager().createPreferenceScreen(getContext());
 
-            PreferenceCategory preferenceCategory = new PreferenceCategory(getContext());
-            preferenceCategory.setTitle(R.string.PREFERENCE_category_notifications);
+        // Категория "Уведомления"
+        PreferenceCategory categoryNotifications = createPreferenceCategory(
+                "Уведомления",
+                preferenceScreen
+        );
+        preferenceScreen.addPreference(categoryNotifications);
 
-            preferenceScreen.addPreference(preferenceCategory);
+        // Переключатель "Карточки"
+        String cardsNotificationsKey = "notify_about_new_cards";
+        SwitchPreference cardsSwitch = createSwitchPreference(
+                cardsNotificationsKey,
+                "О новых карточках",
+                "Уведомлять о новых карточках",
+                false
+        );
+        categoryNotifications.addPreference(cardsSwitch);
 
-            SwitchPreference switchPreference = new SwitchPreference(getContext());
-            switchPreference.setTitle(R.string.PREFERENCE_notify_about_new_cards_title);
-            switchPreference.setSummary(R.string.PREFERENCE_notify_about_new_cards_description);
-            switchPreference.setKey(cardsNotificationsPrefKey);
-            switchPreference.setDefaultValue(false);
 
-            preferenceCategory.addPreference(switchPreference);
+        // Переключатель "Комментарии"
+        String commentsNotificationsKey = "notify_about_new_comments";
+        SwitchPreference commentsSwitch = createSwitchPreference(
+                commentsNotificationsKey,
+                "О новых комментариях",
+                "Уведомлять о новых комментариях",
+                false
+        );
+        categoryNotifications.addPreference(commentsSwitch);
 
-            setPreferenceScreen(preferenceScreen);
-        }
+        // Отображаю настройки
+        setPreferenceScreen(preferenceScreen);
     }
 
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
 
+    }
+
+
+    private PreferenceCategory createPreferenceCategory(String title, PreferenceScreen preferenceScreen) {
+        PreferenceCategory preferenceCategory = new PreferenceCategory(getContext());
+        preferenceCategory.setIconSpaceReserved(false);
+        preferenceCategory.setTitle(title);
+        preferenceScreen.addPreference(preferenceCategory);
+        return preferenceCategory;
+    }
+
+    private SwitchPreference createSwitchPreference(String key, String title, String subtitle, boolean defaultValue) {
+        SwitchPreference switchPreference = new SwitchPreference(getContext());
+        switchPreference.setTitle(title);
+        switchPreference.setSummary(subtitle);
+        switchPreference.setKey(key);
+        switchPreference.setDefaultValue(defaultValue);
+        switchPreference.setIconSpaceReserved(false);
+        return switchPreference;
     }
 }
