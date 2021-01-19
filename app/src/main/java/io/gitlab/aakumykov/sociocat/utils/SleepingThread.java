151f5ba12f42e421dc696ff74327dfb50cc2e022
Andrey Kumykov <aakumykov@yandex.ru>
2021-01-19 17:38:37 +0300

    Вернул случайно удалённый класс SleepingThread.

---
+++ app/src/main/java/io/gitlab/aakumykov/sociocat/utils/SleepingThread.java
@@ -0,0 +1,54 @@
+package io.gitlab.aakumykov.sociocat.utils;
+
+import androidx.annotation.NonNull;
+
+import java.util.concurrent.TimeUnit;
+
+public class SleepingThread extends Thread {
+
+    private int mTimeout;
+    private iSleepingThreadCallbacks callbacks;
+
+    public SleepingThread(
+            int timeToSleep,
+            iSleepingThreadCallbacks DSTCallbacks
+    ) {
+        mTimeout = timeToSleep;
+        callbacks = DSTCallbacks;
+    }
+
+    @Override
+    public synchronized void start() {
+        super.start(); // ?
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                callbacks.onSleepingStart();
+
+                while (mTimeout >= 0 && !callbacks.isReadyToWakeUpNow()) {
+                    try {
+                        TimeUnit.SECONDS.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    callbacks.onSleepingTick(mTimeout);
+
+                    mTimeout--;
+                }
+
+                callbacks.onSleepingEnd();
+            }
+        }).start();
+    }
+
+    public interface iSleepingThreadCallbacks {
+        void onSleepingStart();
+        void onSleepingTick(int secondsToWakeUp);
+        void onSleepingEnd();
+        boolean isReadyToWakeUpNow();
+        void onSleepingError(@NonNull String errorMsg);
+    }
+}
+
