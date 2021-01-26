2cb52dfa72209b09ddbfa2cb316630156e1b358e
Andrey Kumykov <aakumykov@yandex.ru>
2021-01-26 23:50:18 +0300

    Новый Google web client id.

--- app/src/main/java/ru/aakumykov/me/sociocat/AuthConfig.java
+++ app/src/main/java/ru/aakumykov/me/sociocat/AuthConfig.java
@@ -4,5 +4,5 @@ public final class AuthConfig {
     private AuthConfig(){}
 
     public static final String GOOGLE_WEB_CLIENT_ID =
-            "161012924326-jvcqjlunkqqkrh813bqcmttiicj5i77h.apps.googleusercontent.com";
+            "161012924326-omh2jjisomjnsbicsifpf0cj42382e46.apps.googleusercontent.com";
 }
