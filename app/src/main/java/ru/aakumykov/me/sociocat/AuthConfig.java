fdf87c7a72109a5dbe4babacc559dec48d74fcec
Andrey Kumykov <aakumykov@yandex.ru>
2021-01-26 21:25:29 +0300

    Новый Google WebClient id.

--- app/src/main/java/ru/aakumykov/me/sociocat/AuthConfig.java
+++ app/src/main/java/ru/aakumykov/me/sociocat/AuthConfig.java
@@ -3,5 +3,6 @@ package ru.aakumykov.me.sociocat;
 public final class AuthConfig {
     private AuthConfig(){}
 
-    public static final String GOOGLE_WEB_CLIENT_ID = "161012924326-vjltklgj50uhkb942ro0nmst7pujf5ov.apps.googleusercontent.com";
+    public static final String GOOGLE_WEB_CLIENT_ID =
+            "161012924326-jvcqjlunkqqkrh813bqcmttiicj5i77h.apps.googleusercontent.com";
 }
